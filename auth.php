2b03b2533546a5a33d5e38ff179bfef266d7435b
Ricardo Nishimura <ricnishimura@gmail.com>
2016-06-27 23:37:23 -0300

    Code simplification.

--- auth.php
+++ auth.php
@@ -3,42 +3,42 @@ session_start();
 
 function authHTML()
 {
-  // if not auhtenticaded session go to login.php
-  if(empty($_SESSION['userlogin']) || empty($_SESSION['authenticated']))
-  {
-      header('Location: login.php');
-      exit();
-  }
+    // 	if not auhtenticaded session go to login.php
+    if (empty($_SESSION['userlogin']) || empty($_SESSION['authenticated'])) {
+        header('Location: login.php');
+        exit();
+    }
 }
 
 function authAPI()
 {
-  $user = isset($_SERVER['PHP_AUTH_USER'])?$_SERVER['PHP_AUTH_USER']:'';
-  $pass = isset($_SERVER['PHP_AUTH_PW'])?$_SERVER['PHP_AUTH_PW']:'';
+    $user = isset($_SERVER['PHP_AUTH_USER']) ? $_SERVER['PHP_AUTH_USER'] : '';
+    $pass = isset($_SERVER['PHP_AUTH_PW']) ? $_SERVER['PHP_AUTH_PW'] : '';
 
-  if(empty($user) || empty($pass) || !isValidyUser($user, $pass))
-  {
-    $_SESSION['authenticated_api'] = 0;
-    header('WWW-Authenticate: Basic realm="My Realm"');
-    header('HTTP/1.0 401 Unauthorized');
-    die ("Not authorized");
-  }
+    if (empty($user) || empty($pass) || !isValidUser($user, $pass)) {
+        $_SESSION['authenticated_api'] = 0;
+        header('WWW-Authenticate: Basic realm="My Realm"');
+        header('HTTP/1.0 401 Unauthorized');
+        die("Not authorized");
+    }
 
-  $_SESSION['userlogin'] = $user;
-  $_SESSION['authenticated_api'] = 1;
+    $_SESSION['userlogin'] = $user;
+    $_SESSION['authenticated_api'] = 1;
 }
 
-function isValidyUser($user, $pass)
+function isValidUser($user, $pass)
 {
-  if(!empty($_SESSION['authenticated_api']) && !empty($_SESSION['userlogin']) && $_SESSION['userlogin'] === $user)
-  {
-    return true;
-  }
+    if (!empty($_SESSION['authenticated_api']) &&
+        !empty($_SESSION['userlogin']) &&
+        $_SESSION['userlogin'] === $user) {
+        return true;
+    }
 
-  // Implement validation: db, array, whatever...
-  if($user === 'admin' && $pass === 'admin')
-  {
-    return true;
-  }
-  return false;
+
+    // 	Implement validation: db, array, whatever...
+    if ($user === 'admin' && $pass === 'admin') {
+        return true;
+    }
+
+    return false;
 }
--- login.php
+++ login.php
@@ -1,32 +1,65 @@
 <?php
-  // The user login page
-  include 'templates/header.php';
+require_once 'auth.php';
+
+// If post, check user
+if (isset($_POST['login'])) {
+    if (!empty($_POST['userlogin']) && !empty($_POST['pass'])) {
+        // Is already authenticated
+        if (!empty($_SESSION['authenticated'])) {
+            // Is new user different from current
+            if ($_POST['userlogin'] !== $_SESSION['userlogin']) {
+                // Check user again
+                $_SESSION['authenticated'] = FALSE;
+            }
+        }
+
+        // User not logged
+        if (empty($_SESSION['authenticated'])) {
+            // Check user validity
+            if (isValidUser($_POST['userlogin'], $_POST['pass'])) {
+                // Log in
+                $_SESSION['userlogin'] = $_POST['userlogin'];
+                $_SESSION['authenticated'] = TRUE;
+            }
+        }
+    }
+}
+
+// If already authenticated, route to home page
+if (!empty($_SESSION['authenticated'])) {
+    header('Location: index.php');
+    exit();
+}
+
+// The user login page
+include 'templates/header.php';
+
 ?>
 
-	<!--login modal-->
-	<div id="loginModal" class="modal show bs-example-modal-sm" tabindex="-1" role="dialog" aria-hidden="true">
-	  <div class="modal-dialog modal-sm">
-	  <div class="modal-content">
-		  <form class="form" method="POST" action="auth_login.php">
-		  <div class="modal-header">
-			  <h3 class="text-center">User authentication</h3>
-		  </div>
-		  <div class="modal-body">
-				<div class="form-group">
-				  <input type="text" class="form-control input-sm" placeholder="login" name="userlogin">
-				</div>
-				<div class="form-group">
-				  <input type="password" class="form-control input-sm" placeholder="password" name="pass">
-          <input type="hidden" name="login" value="login">
-				</div>
-		  </div>
-		  <div class="modal-footer">
-				  <button class="btn btn-default">Login</button>
-		  </div>
-		  </form>
-	  </div>
-	  </div>
-	</div>
+<!--login modal-->
+<div id="loginModal" class="modal show bs-example-modal-sm" tabindex="-1" role="dialog" aria-hidden="true">
+    <div class="modal-dialog modal-sm">
+        <div class="modal-content">
+            <form class="form" method="POST" action="login.php">
+                <div class="modal-header">
+                    <h3 class="text-center">User authentication</h3>
+                </div>
+                <div class="modal-body">
+                    <div class="form-group">
+                        <input type="text" class="form-control input-sm" placeholder="login" name="userlogin">
+                    </div>
+                    <div class="form-group">
+                        <input type="password" class="form-control input-sm" placeholder="password" name="pass">
+                        <input type="hidden" name="login" value="login">
+                    </div>
+                </div>
+                <div class="modal-footer">
+                    <button class="btn btn-default">Login</button>
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
 
 <?php
-  include 'templates/footer.php';
+include 'templates/footer.php';
