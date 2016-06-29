8c893fa740ba0f41e68310c13237ded201762637
Ricardo Nishimura <ricnishimura@gmail.com>
2016-06-28 22:17:11 -0300

    Code simplification.

--- auth.php
+++ auth.php
@@ -4,7 +4,7 @@ session_start();
 function authHTML()
 {
     // 	if not auhtenticaded session go to login.php
-    if (empty($_SESSION['userlogin']) || empty($_SESSION['authenticated'])) {
+    if (empty($_SESSION['userlogin'])) {
         header('Location: login.php');
         exit();
     }
@@ -15,30 +15,22 @@ function authAPI()
     $user = isset($_SERVER['PHP_AUTH_USER']) ? $_SERVER['PHP_AUTH_USER'] : '';
     $pass = isset($_SERVER['PHP_AUTH_PW']) ? $_SERVER['PHP_AUTH_PW'] : '';
 
-    if (empty($user) || empty($pass) || !isValidUser($user, $pass)) {
-        $_SESSION['authenticated_api'] = 0;
+    if (!isValidUser($user, $pass)) {
+        $_SESSION['userlogin'] = FALSE;
         header('WWW-Authenticate: Basic realm="My Realm"');
         header('HTTP/1.0 401 Unauthorized');
         die("Not authorized");
     }
 
     $_SESSION['userlogin'] = $user;
-    $_SESSION['authenticated_api'] = 1;
 }
 
 function isValidUser($user, $pass)
 {
-    if (!empty($_SESSION['authenticated_api']) &&
-        !empty($_SESSION['userlogin']) &&
-        $_SESSION['userlogin'] === $user) {
-        return true;
-    }
-
-
     // 	Implement validation: db, array, whatever...
     if ($user === 'admin' && $pass === 'admin') {
-        return true;
+        return TRUE;
     }
 
-    return false;
+    return FALSE;
 }
