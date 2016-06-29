c234279dc4fea5128172a0247fb4f7e08204995a
Ricardo Nishimura <ricnishimura@gmail.com>
2016-06-28 22:12:38 -0300

    Code simplification.

--- login.php
+++ login.php
@@ -2,38 +2,22 @@
 require_once 'auth.php';
 
 // If post, check user
-if (isset($_POST['login'])) {
-    if (!empty($_POST['userlogin']) && !empty($_POST['pass'])) {
-        // Is already authenticated
-        if (!empty($_SESSION['authenticated'])) {
-            // Is new user different from current
-            if ($_POST['userlogin'] !== $_SESSION['userlogin']) {
-                // Check user again
-                $_SESSION['authenticated'] = FALSE;
-            }
-        }
-
-        // User not logged
-        if (empty($_SESSION['authenticated'])) {
-            // Check user validity
-            if (isValidUser($_POST['userlogin'], $_POST['pass'])) {
-                // Log in
-                $_SESSION['userlogin'] = $_POST['userlogin'];
-                $_SESSION['authenticated'] = TRUE;
-            }
-        }
+if (!empty($_POST['userlogin']) && !empty($_POST['pass'])) {
+    // Verify user and password
+    if (isValidUser($_POST['userlogin'], $_POST['pass'])) {
+        // Log in
+        $_SESSION['userlogin'] = $_POST['userlogin'];
+        header('Location: index.php');
+        exit();
     }
-}
-
-// If already authenticated, route to home page
-if (!empty($_SESSION['authenticated'])) {
-    header('Location: index.php');
-    exit();
+    else
+    {
+        $_SESSION['userlogin'] = FALSE;
+    }
 }
 
 // The user login page
 include 'templates/header.php';
-
 ?>
 
 <!--login modal-->
@@ -50,7 +34,6 @@ include 'templates/header.php';
                     </div>
                     <div class="form-group">
                         <input type="password" class="form-control input-sm" placeholder="password" name="pass">
-                        <input type="hidden" name="login" value="login">
                     </div>
                 </div>
                 <div class="modal-footer">
